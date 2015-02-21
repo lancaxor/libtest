bdb36e6d9a8937d6e2a7c523657e83f624174627
lexis <alex.layt.pro@gmail.com>
2015-02-21 16:42:21 +0200

    a little classname edit

---
+++ libTestApp/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace libTestApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idMainCall = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // idMainCall
+            // 
+            this.idMainCall.Location = new System.Drawing.Point(233, 12);
+            this.idMainCall.Name = "idMainCall";
+            this.idMainCall.Size = new System.Drawing.Size(85, 35);
+            this.idMainCall.TabIndex = 0;
+            this.idMainCall.Text = "Call Main";
+            this.idMainCall.UseVisualStyleBackColor = true;
+            this.idMainCall.Click += new System.EventHandler(this.idMainCall_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 57);
+            this.Controls.Add(this.idMainCall);
+            this.Name = "MainForm";
+            this.Text = "Main Tes Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button idMainCall;
+    }
+}
+
---
+++ libTestApp/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Runtime;
+using testLib;
+
+namespace libTestApp
+{
+    public partial class MainForm : Form
+    {
+        //[DllImport("testLib.dll")]
+        //extern static void SomeFoo();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void idMainCall_Click(object sender, EventArgs e)
+        {
+            HTMLWorker.SomeFoo(delegate() { MessageBox.Show("called from DLL!!! ^.^"); });
+        }
+    }
+}
