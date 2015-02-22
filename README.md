8d769712bccd755d294183160513f47ba237b7b3
lexis <alex.layt.pro@gmail.com>
2015-02-22 02:05:57 +0200

    Added readme

---
+++ README.md
@@ -0,0 +1,6 @@
+Just a project for testing DLL using.
+testLib -- DLL
+libTestApp -- WinForms app for DLL testing
+testService -- Windows Service using DLL
+
+And additionally -- a little post-build script in testLib/scripts.
--- libTestApp/MainForm.Designer.cs
+++ libTestApp/MainForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.idMainCall = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // idMainCall
@@ -41,11 +42,21 @@
             this.idMainCall.UseVisualStyleBackColor = true;
             this.idMainCall.Click += new System.EventHandler(this.idMainCall_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(330, 57);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.idMainCall);
             this.Name = "MainForm";
             this.Text = "Main Tes Form";
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Button idMainCall;
+        private System.Windows.Forms.Button button1;
     }
 }
 
